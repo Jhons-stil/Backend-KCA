0054aa624677be8b26ea87a558da26221d02f6b7
Haikal <haikalhakam422@gmail.com>
2026-05-04 06:14:06 +0700

    memodifikasi file app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 process.env.TZ = "Asia/Jakarta";
 const express = require("express");
+const multer = require("multer");
 const cors = require("cors");
+const path = require("path");
 const routerUser = require("./http/user/router.js");
 const routerFinance = require("./http/finance/router.js");
 
@@ -9,6 +11,19 @@ const routerDashboard = require("./http/dashboard/route.js");
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("trust proxy", true);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.use((err, req, res, next) => {
+  if (err instanceof multer.MulterError) {
+    console.error("Multer error:", err);
+    return res.status(400).json({ status: "error", message: err.message });
+  }
+  next(err);
+});
 
 app.use(cors());
 app.use(express.json());
